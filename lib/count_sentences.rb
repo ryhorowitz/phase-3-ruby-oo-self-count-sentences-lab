ef373cfbca937fb5d6785f1a579f509ed5d82c61
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-03-20 18:45:48 -0400

    commit tree 46e53a2048e17d02aad66101497433f06454911f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
-require 'pry'
+require "pry"
 
 class String
-
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_array = self.split(/[!.?]/)
+    split_array.delete_if { |x| x == "" }
+    split_array.size
   end
 end
